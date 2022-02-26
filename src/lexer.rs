363b1a2938a4e8e725f66a6d52c2cf84f1e88990
ryuryu-ymj <33318253+ryuryu-ymj@users.noreply.github.com>
2022-02-26 22:56:16 +0900

    move test_lexer.rs to lexer directory

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,6 +2,9 @@ use crate::token::Token;
 use std::iter::Peekable;
 use std::str::CharIndices;
 
+#[cfg(test)]
+mod test_lexer;
+
 pub struct Lexer<'input> {
     input: &'input str,
     chars: Peekable<CharIndices<'input>>,
@@ -69,80 +72,6 @@ impl<'input> Lexer<'input> {
                         return Token::Ident(&self.input[pos..*j]);
                     }
                 },
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-mod text_lexer {
-    use super::*;
-    use crate::token::Token;
-
-    #[test]
-    fn test_next_token() {
-        let tests = [
-            (
-                "10+2*393",
-                vec![
-                    Token::Num("10"),
-                    Token::Plus,
-                    Token::Num("2"),
-                    Token::Asterisk,
-                    Token::Num("393"),
-                ],
-            ),
-            (
-                "\t10  +  2 * 393",
-                vec![
-                    Token::Num("10"),
-                    Token::Plus,
-                    Token::Num("2"),
-                    Token::Asterisk,
-                    Token::Num("393"),
-                ],
-            ),
-            (
-                "(10 - 2) / 393",
-                vec![
-                    Token::Lparen,
-                    Token::Num("10"),
-                    Token::Minus,
-                    Token::Num("2"),
-                    Token::Rparen,
-                    Token::Slash,
-                    Token::Num("393"),
-                ],
-            ),
-            (
-                "ab c*de",
-                vec![
-                    Token::Ident("a"),
-                    Token::Ident("b"),
-                    Token::Ident("c"),
-                    Token::Asterisk,
-                    Token::Ident("d"),
-                    Token::Ident("e"),
-                ],
-            ),
-            (
-                r"\sin(3\pi a)",
-                vec![
-                    Token::Ident(r"\sin"),
-                    Token::Lparen,
-                    Token::Num("3"),
-                    Token::Ident(r"\pi"),
-                    Token::Ident("a"),
-                    Token::Rparen,
-                ],
-            ),
-        ];
-
-        for (input, expected) in tests {
-            let mut l = Lexer::new(input);
-            for e in expected {
-                let tok = l.next_token();
-                assert_eq!(tok.unwrap(), e);
             }
         }
     }
---
+++ src/lexer/test_lexer.rs
@@ -0,0 +1,70 @@
+use super::*;
+use crate::token::Token;
+
+#[test]
+fn test_next_token() {
+    let tests = [
+        (
+            "10+2*393",
+            vec![
+                Token::Num("10"),
+                Token::Plus,
+                Token::Num("2"),
+                Token::Asterisk,
+                Token::Num("393"),
+            ],
+        ),
+        (
+            "\t10  +  2 * 393",
+            vec![
+                Token::Num("10"),
+                Token::Plus,
+                Token::Num("2"),
+                Token::Asterisk,
+                Token::Num("393"),
+            ],
+        ),
+        (
+            "(10 - 2) / 393",
+            vec![
+                Token::Lparen,
+                Token::Num("10"),
+                Token::Minus,
+                Token::Num("2"),
+                Token::Rparen,
+                Token::Slash,
+                Token::Num("393"),
+            ],
+        ),
+        (
+            "ab c*de",
+            vec![
+                Token::Ident("a"),
+                Token::Ident("b"),
+                Token::Ident("c"),
+                Token::Asterisk,
+                Token::Ident("d"),
+                Token::Ident("e"),
+            ],
+        ),
+        (
+            r"\sin(3\pi a)",
+            vec![
+                Token::Ident(r"\sin"),
+                Token::Lparen,
+                Token::Num("3"),
+                Token::Ident(r"\pi"),
+                Token::Ident("a"),
+                Token::Rparen,
+            ],
+        ),
+    ];
+
+    for (input, expected) in tests {
+        let mut l = Lexer::new(input);
+        for e in expected {
+            let tok = l.next_token();
+            assert_eq!(tok.unwrap(), e);
+        }
+    }
+}
