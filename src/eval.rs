e5c2b719fecf0b7ab27abc566461cce0476047fd
ryuryu-ymj <33318253+ryuryu-ymj@users.noreply.github.com>
2022-03-10 19:05:44 +0900

    flatten add, mul expr when evaluating

--- src/eval.rs
+++ src/eval.rs
@@ -2,12 +2,40 @@ use crate::{ast, object};
 
 const NEG_ONE: object::Expr = object::Expr::Num(-1);
 
-macro_rules! add {
-    ( $( $e:expr ),* ) => ( object::Expr::Add(vec![ $( $e ),* ]) )
+fn add(left: object::Expr, right: object::Expr) -> object::Expr {
+    match (left, right) {
+        (object::Expr::Add(mut v1), object::Expr::Add(mut v2)) => {
+            v1.append(&mut v2);
+            object::Expr::Add(v1)
+        }
+        (object::Expr::Add(mut v), e) => {
+            v.push(e);
+            object::Expr::Add(v)
+        }
+        (e, object::Expr::Add(mut v)) => {
+            v.push(e);
+            object::Expr::Add(v)
+        }
+        (l, r) => object::Expr::Add(vec![l, r]),
+    }
 }
 
-macro_rules! mul {
-    ( $( $e:expr ),* ) => ( object::Expr::Mul(vec![ $( $e ),* ]) )
+fn mul(left: object::Expr, right: object::Expr) -> object::Expr {
+    match (left, right) {
+        (object::Expr::Mul(mut v1), object::Expr::Mul(mut v2)) => {
+            v1.append(&mut v2);
+            object::Expr::Mul(v1)
+        }
+        (object::Expr::Mul(mut v), e) => {
+            v.push(e);
+            object::Expr::Mul(v)
+        }
+        (e, object::Expr::Mul(mut v)) => {
+            v.push(e);
+            object::Expr::Mul(v)
+        }
+        (l, r) => object::Expr::Mul(vec![l, r]),
+    }
 }
 
 fn pow(base: object::Expr, power: object::Expr) -> object::Expr {
@@ -22,22 +50,18 @@ pub fn eval_expr(e: ast::Expr) -> object::Expr {
         }
         ast::Expr::Ident(s) => object::Expr::Ident(s.to_string()),
         ast::Expr::Unary(op, expr) => match op {
-            ast::UnOp::Neg => {
-                mul!(NEG_ONE, eval_expr(*expr))
-            }
+            ast::UnOp::Neg => mul(NEG_ONE, eval_expr(*expr)),
         },
         ast::Expr::Binary(op, left, right) => match op {
-            ast::BinOp::Add => {
-                add!(eval_expr(*left), eval_expr(*right))
-            }
+            ast::BinOp::Add => add(eval_expr(*left), eval_expr(*right)),
             ast::BinOp::Sub => {
-                add!(eval_expr(*left), mul!(NEG_ONE, eval_expr(*right)))
+                add(eval_expr(*left), mul(NEG_ONE, eval_expr(*right)))
             }
             ast::BinOp::Mul | ast::BinOp::ImpliedMul => {
-                mul!(eval_expr(*left), eval_expr(*right))
+                mul(eval_expr(*left), eval_expr(*right))
             }
             ast::BinOp::Div => {
-                mul!(eval_expr(*left), pow(eval_expr(*right), NEG_ONE))
+                mul(eval_expr(*left), pow(eval_expr(*right), NEG_ONE))
             }
         },
     }
