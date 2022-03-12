09a0cbcf1e28c491e90f973431a1914e5d84ffa1
ryuryu-ymj <33318253+ryuryu-ymj@users.noreply.github.com>
2022-03-12 19:18:16 +0900

    refactoring

--- src/eval.rs
+++ src/eval.rs
@@ -5,6 +5,31 @@ mod test;
 
 const NEG_ONE: object::Expr = object::Expr::Num(-1);
 
+pub fn eval_expr(e: ast::Expr) -> object::Expr {
+    match e {
+        ast::Expr::Num(s) => {
+            let n: i32 = s.parse().unwrap();
+            object::Expr::Num(n)
+        }
+        ast::Expr::Ident(s) => object::Expr::Ident(s.to_string()),
+        ast::Expr::Unary(op, expr) => match op {
+            ast::UnOp::Neg => mul(NEG_ONE, eval_expr(*expr)),
+        },
+        ast::Expr::Binary(op, left, right) => match op {
+            ast::BinOp::Add => add(eval_expr(*left), eval_expr(*right)),
+            ast::BinOp::Sub => {
+                add(eval_expr(*left), mul(NEG_ONE, eval_expr(*right)))
+            }
+            ast::BinOp::Mul | ast::BinOp::ImpliedMul => {
+                mul(eval_expr(*left), eval_expr(*right))
+            }
+            ast::BinOp::Div => {
+                mul(eval_expr(*left), pow(eval_expr(*right), NEG_ONE))
+            }
+        },
+    }
+}
+
 fn add(left: object::Expr, right: object::Expr) -> object::Expr {
     match (left, right) {
         (object::Expr::Add(mut v1), object::Expr::Add(mut v2)) => {
@@ -43,29 +68,4 @@ fn mul(left: object::Expr, right: object::Expr) -> object::Expr {
 
 fn pow(base: object::Expr, power: object::Expr) -> object::Expr {
     object::Expr::Pow(Box::new(base), Box::new(power))
-}
-
-pub fn eval_expr(e: ast::Expr) -> object::Expr {
-    match e {
-        ast::Expr::Num(s) => {
-            let n: i32 = s.parse().unwrap();
-            object::Expr::Num(n)
-        }
-        ast::Expr::Ident(s) => object::Expr::Ident(s.to_string()),
-        ast::Expr::Unary(op, expr) => match op {
-            ast::UnOp::Neg => mul(NEG_ONE, eval_expr(*expr)),
-        },
-        ast::Expr::Binary(op, left, right) => match op {
-            ast::BinOp::Add => add(eval_expr(*left), eval_expr(*right)),
-            ast::BinOp::Sub => {
-                add(eval_expr(*left), mul(NEG_ONE, eval_expr(*right)))
-            }
-            ast::BinOp::Mul | ast::BinOp::ImpliedMul => {
-                mul(eval_expr(*left), eval_expr(*right))
-            }
-            ast::BinOp::Div => {
-                mul(eval_expr(*left), pow(eval_expr(*right), NEG_ONE))
-            }
-        },
-    }
 }
